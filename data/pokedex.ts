a16658b67ec2c6c346977e509b6dced9aaf92aaa
F2PAGL <angel1esoee@gmail.com>
2026-01-31 13:24:45 +0100

    Update pokedex.ts

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -1032,9 +1032,9 @@ export const Pokedex: import('../sim/dex-species').SpeciesDataTable = {
 	ninetales: {
 		num: 38,
 		name: "Ninetales",
-		types: ["Fire"],
+		types: ["Fire", "Fairy"],
 		genderRatio: { M: 0.25, F: 0.75 },
-		baseStats: { hp: 73, atk: 76, def: 75, spa: 81, spd: 100, spe: 100 },
+		baseStats: { hp: 73, atk: 50, def: 75, spa: 105, spd: 100, spe: 102 },
 		abilities: { 0: "Flash Fire", H: "Drought" },
 		heightm: 1.1,
 		weightkg: 19.9,
