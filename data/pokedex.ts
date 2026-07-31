62eb6d8bfa0464043d7e1d9bb301fd402793b81f
F2PAGL <angel1esoee@gmail.com>
2026-07-31 14:43:45 +0200

    Update pokedex.ts

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -2418,7 +2418,7 @@ export const Pokedex: import('../sim/dex-species').SpeciesDataTable = {
 		name: "Hitmonlee",
 		types: ["Fighting"],
 		gender: "M",
-		baseStats: { hp: 50, atk: 120, def: 53, spa: 35, spd: 110, spe: 87 },
+		baseStats: { hp: 60, atk: 120, def: 53, spa: 35, spd: 110, spe: 92 },
 		abilities: { 0: "Limber", 1: "Reckless", H: "Unburden" },
 		heightm: 1.5,
 		weightkg: 49.8,
